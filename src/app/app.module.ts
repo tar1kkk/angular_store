bcd01ce474c85c939a7767557ede75660ca4e668
Taras <81424893+tar1kkk@users.noreply.github.com>
2023-03-17 23:51:41 +0200

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/UI/header/header.component';
+import { FooterComponent } from './components/UI/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { BaseComponent } from './components/base/base.component';
+import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    BasketComponent,
+    ProductDetailsComponent,
+    BaseComponent,
+    DialogBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
